ce13bbcf5bc2a0ae5bc03d5ea52d8aab05fdc5e4
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-10-14 15:53:57 +0200

    Add project files.

---
+++ LexiconProject_ComputerWebShop/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconProject_ComputerWebShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
